36c8356b4627042049bec5329601651715e4d044
PMohanReddy123 <45960078+PMohanReddy123@users.noreply.github.com>
2019-11-06 17:44:10 +0530

    Update RDS_Add_Tags.py

--- RDS_Add_Tags.py
+++ RDS_Add_Tags.py
@@ -3,7 +3,7 @@ import boto3
 client = boto3.client('rds')
 
 response = client.add_tags_to_resource(
-    ResourceName='arn:aws:rds:eu-west-1:378457291432:db:afrprdcst01',
+    ResourceName='',
     Tags=[
         {
             'Key': 'Name',
