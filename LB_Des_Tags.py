7ce3e7ceae7117e01bebff921bcea6c3490601a2
PMohanReddy123 <45960078+PMohanReddy123@users.noreply.github.com>
2019-11-06 17:50:40 +0530

    Update and rename lb_des_tags.py to LB_Des_Tags.py

--- LB_Des_Tags.py
+++ LB_Des_Tags.py
@@ -3,7 +3,7 @@ client = boto3.client('elbv2')
 
 response = client.describe_tags(
     ResourceArns=[
-        'arn:aws:elasticloadbalancing:eu-west-1:378457291432:loadbalancer/app/cbsextprod/d091053ef96a5b44',
+        'LB_ARN',
     ]
 )
 for resp in response['TagDescriptions']:
