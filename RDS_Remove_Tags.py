ecc00b199866da1812fc1ddb2bfd06633ad24449
PMohanReddy123 <45960078+PMohanReddy123@users.noreply.github.com>
2019-11-06 17:44:51 +0530

    Update RDS_Remove_Tags.py

--- RDS_Remove_Tags.py
+++ RDS_Remove_Tags.py
@@ -3,7 +3,7 @@ import boto3
 client = boto3.client('rds')
 
 response = client.remove_tags_from_resource(
-    ResourceName='arn:aws:rds:eu-west-1:378457291432:db:afrprdcst01',
+    ResourceName='',
     TagKeys=[
         'Name',
     ]
