8c03828fe20774b820924b1893cbc74b583b505b
PMohanReddy123 <45960078+PMohanReddy123@users.noreply.github.com>
2019-11-06 17:54:56 +0530

    Update and rename lista.py to List_Instance & volumes.py

--- List_Instance & volumes.py
+++ List_Instance & volumes.py
@@ -24,9 +24,9 @@ for reservation in resp['Reservations']:
     for instance in reservation['Instances']:
         instance_id = instance['InstanceId']
         state = instance['State']['Name']
-        # for volume in instance['BlockDeviceMappings']:
-        #     vol_id = volume['Ebs']['VolumeId']
-        #     vol_status=volume['Ebs']['Status']
-        #     vol_attach_time=volume['Ebs']['AttachTime']
-        #     print('Instance Id = {} And Its State is {} and volume id is {} and its status is {} and its attached time is {}'.format(instance_id,state,vol_id,vol_status,vol_attach_time))
+        for volume in instance['BlockDeviceMappings']:
+            vol_id = volume['Ebs']['VolumeId']
+            vol_status=volume['Ebs']['Status']
+            vol_attach_time=volume['Ebs']['AttachTime']
+            print('Instance Id = {} And Its State is {} and volume id is {} and its status is {} and its attached time is {}'.format(instance_id,state,vol_id,vol_status,vol_attach_time))
         print("id is{}, state is {}".format(instance_id,state))
