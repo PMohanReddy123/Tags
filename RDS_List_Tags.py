9cb129bea16f99263b0e3e06eea6e112e895c0e2
PMohanReddy123 <45960078+PMohanReddy123@users.noreply.github.com>
2019-11-06 17:44:37 +0530

    Update RDS_List_Tags.py

--- RDS_List_Tags.py
+++ RDS_List_Tags.py
@@ -3,7 +3,7 @@ import boto3
 client = boto3.client('rds')
 
 response = client.list_tags_for_resource(
-    ResourceName='arn:aws:rds:eu-west-1:378457291432:db:afrprdcst01',
+    ResourceName='',
     # Filters=[
     #     {
     #         'Name': 'workload-type',
